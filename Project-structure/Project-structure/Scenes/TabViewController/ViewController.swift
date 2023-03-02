210785adfb2290bdea69da55f2df4fde0062c183
Lera <lera.onishchenko05@gmail.com>
2023-03-02 16:26:19 +0200

    created tab bar

--- Project-structure/Project-structure/Scenes/TabViewController/ViewController.swift
+++ Project-structure/Project-structure/Scenes/TabViewController/ViewController.swift
@@ -11,7 +11,32 @@ class ViewController: UITabBarController, StoryboardInitializable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateTabBar()
     }
-
+    
+    
+    private func generateTabBar(){
+        viewControllers = [
+            generateVC(
+                viewController: WalkViewController(),
+                image:  UIImage(named: "PointAnnotation")
+        ),
+            generateVC(
+                viewController: MapViewController(),
+                image: UIImage(named: "WalkAnnotation")
+                
+            ),
+            generateVC(
+                viewController: StatisticsViewController(),
+                image: UIImage(named: "StatisticAnnotation")
+            )
+        ]
+    }
+    
+    
+    private func generateVC(viewController: UIViewController, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.image = image
+        return viewController
+    }
 }
 
